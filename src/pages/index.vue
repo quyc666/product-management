a91939b416c9c9cb0c3a7d0d88d9d8782b9b5875
quyc <a94251049@gmail.com>
2024-12-22 10:39:27 +0800

    feat: statistical information using skeleton

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,26 @@
 <template>
     <div class="mx-5">
         <el-row :gutter="20">
+            <el-col v-if="panels.length == 0" :span="6" v-for="i in 4" :key="i">
+                <el-skeleton style="width: 100%">
+                    <template #template>
+                        <el-card shadow="hover" class="border-0">
+                            <template #header>
+                                <div class="flex justify-between">
+                                    <el-skeleton-item variant="text" style="width: 50%" />
+                                    <el-skeleton-item variant="text" style="width: 10%" />
+                                </div>
+                            </template>
+                            <el-skeleton-item variant="text" style="width: 30%" />
+                            <el-divider />
+                            <div class="flex justify-between">
+                                <el-skeleton-item variant="text" style="width: 50%" />
+                                <el-skeleton-item variant="text" style="width: 10%" />
+                            </div>
+                        </el-card>
+                    </template>
+                </el-skeleton>
+            </el-col>
             <el-col :span="6" :offset="0" v-for="(item, index) in panels" :key="index">
                 <el-card shadow="hover" class="border-0">
                     <template #header>
@@ -12,8 +32,8 @@
                         </div>
                     </template>
                     <span class="text-3xl font-bold text-gray-500">{{ item.value }}</span>
-                     <el-divider />
-                    <div class="flex justify-between">
+                    <el-divider />
+                    <div class="flex justify-between text-sm text-gray-500">
                         <span>{{ item.subTitle }}</span>
                         <span>{{ item.subValue }}</span>
                     </div>

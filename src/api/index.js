b99e25e24532c2b86327927b94b3de876c1c6191
quyc <a94251049@gmail.com>
2024-12-25 16:30:11 +0800

    feat: the prompt at the bottom of the index includes goods and orders

--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,10 @@ export function getstatistics1(){
     return axios.get('/admin/statistics1')
 }
 
+export function getstatistics2(){
+    return axios.get('/admin/statistics2')
+}
+
 export function getstatistics3(type){
     return axios.get('/admin/statistics3?type='+type)
 }
